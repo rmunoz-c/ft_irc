ed25219bf6cd0b84da48bdd414013c0a2b9d640c
rmunoz-c <rq.munoz.nsp@gmail.com>
2025-12-05 19:46:03 +0100

    Add connection management and activity tracking methods to ClientConnection; update includes in Server.cpp

--- ircserv/src/client/ClientConnection.cpp
+++ ircserv/src/client/ClientConnection.cpp
@@ -25,10 +25,119 @@ ClientConnection::~ClientConnection()
 }
 
 // ========================================================================
+// 							 Connection State
+// ========================================================================
+
+bool ClientConnection::isRegistered() const
+{
+	return _registered;
+}
+
+void ClientConnection::setRegistered(bool r)
+{
+	_registered = r;
+}
+
+void ClientConnection::markPassReceived()
+{
+	_hasSentPass = true;
+}
+
+bool ClientConnection::hasSentPass() const
+{
+	return _hasSentPass;
+}
+
+// ========================================================================
 // 							 	  Socket Info
 // ========================================================================
 
 int ClientConnection::getFd() const
 {
 	return _fd;
+}
+
+// ========================================================================
+// 							  IO Operations
+// ========================================================================
+
+void ClientConnection::appendRecvData(const std::string& data)
+{
+	_recvBuffer += data; 
+}
+
+bool ClientConnection::hasCompleteLine() const
+{
+	return _recvBuffer.find("\r\n") != std::string::npos;
+}
+
+std::string ClientConnection::popLine()
+{
+	size_t pos = _recvBuffer.find("\r\n");
+	std::string line = _recvBuffer.substr(0, pos);
+	_recvBuffer.erase(0, pos + 2);
+
+	return line;
+}
+
+void ClientConnection::queueSend(const std::string& data)
+{
+	_sendBuffer += data;
+}
+
+bool ClientConnection::hasPendingSend() const
+{
+	return !_sendBuffer.empty();
+}
+
+const std::string& ClientConnection::getSendBuffer() const
+{
+	return _sendBuffer;
+}
+
+void ClientConnection::clearSentData(size_t bytes)
+{
+	_sendBuffer.erase(0, bytes);
+}
+
+// ========================================================================
+// 							Activity Tracking
+// ========================================================================
+
+void ClientConnection::updateActivity()
+{
+	_lastActivity = std::time(NULL);
+}
+
+time_t ClientConnection::getLastActivity() const
+{
+	return _lastActivity;
+}
+
+// ========================================================================
+// 						  Connection Management
+// ========================================================================
+
+void ClientConnection::closeConnection()
+{
+	_closed = true;
+}
+
+bool ClientConnection::isClosed() const
+{
+	return _closed;
+}
+
+// ========================================================================
+// 						   User Association
+// ========================================================================
+
+void ClientConnection::setUser(User* user)
+{
+	_user = user;
+}
+
+User* ClientConnection::getUser() const
+{
+	return _user;
 }
